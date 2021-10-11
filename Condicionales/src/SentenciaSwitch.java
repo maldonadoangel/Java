ffc4bcd103a9260fe7e71c52a1087dd6f4054c7c
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-10-11 09:50:13 -0600

    Sentencia Switch

---
+++ Condicionales/src/SentenciaSwitch.java
@@ -0,0 +1,20 @@
+public class SentenciaSwitch {
+    public static void main(String[] args) {
+        int opcion = 9;
+
+        switch(opcion){
+            case 1:
+                System.out.println("opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+            case 3:
+                System.out.println("Opcion 3");
+                break;
+            default:
+                System.out.println("Opcion invalida");
+                break;
+
+        }
+    }
+}
